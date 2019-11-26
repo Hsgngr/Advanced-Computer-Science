6da8e845d399c9f6a88df258622e672bd4c39404
Ege <hsgngr@gmail.com>
2019-11-26 16:04:40 +0000

    A huge commit

--- Project-v3/src/screens/HomeScreen.js
+++ Project-v3/src/screens/HomeScreen.js
@@ -5,6 +5,14 @@ const HomeScreen = props => {
   return (
   <View>
       <Text style={styles.text}>Welcome to the Good Place</Text>
+      <Button
+          onPress= {() => props.navigation.navigate('MapScreen')}
+          title= "Map Demo"
+      />
+      <Button
+          onPress= {() => props.navigation.navigate('FetchScreen')}
+          title= "Fetch Demo"
+      />
       <Button 
         onPress= {() => props.navigation.navigate('Navigation')} 
         title= "Navigation" 
