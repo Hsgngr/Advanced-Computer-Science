12286390d97b2b6ac147c8dbe9388e6ec09331cc
Ege <hsgngr@gmail.com>
2019-11-27 19:18:46 +0000

    SignUp already done

--- task4/src/context/AuthContext.js
+++ task4/src/context/AuthContext.js
@@ -1,17 +1,24 @@
 import {AsyncStorage} from 'react-native';
 import createDataContext from "./createDataContext";
 import trackerApi from '../api/tracker';
-import { navigate} from "../navigationRef";
+import {navigate} from "../navigationRef";
 
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
-            return  {...state, errorMessage: action.payload}; // ...state means take all the properties out of that state object and add them to new this one.
-        case 'signup':
+            return {...state, errorMessage: action.payload}; // ...state means take all the properties out of that state object and add them to new this one.
+        case 'signin': //sonuçta ikisi de aynı şeyi yapıyor farklı farklı switch caselerine gerek yok.
             return {errorMessage: '', token: action.payload}; //Since we signup error message should go away. Restart your errorMessage variable.
+
+        case 'clear_error_message':
+            return{...state, errorMessage:' '}
         default:
             return state;
     }
+};
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({type: 'clear_error_message'})
 };
 
 const signup = dispatch => {
@@ -19,7 +26,7 @@ const signup = dispatch => {
         try {
             const response = await trackerApi.post('/signup', {email, password});
             await AsyncStorage.setItem('token', response.data.token);
-            dispatch({type: 'signup', payload: response.data.token});
+            dispatch({type: 'signin', payload: response.data.token}); //sonuçta ikisi de aynı şeyi yapıyor farklı farklı switch caselerine gerek yok.
 
             navigate('mainFlow');
         } catch (err) {
@@ -29,12 +36,19 @@ const signup = dispatch => {
 };
 
 const signin = dispatch => {
-    return ({email, password}) => {
-        //make api request to sign in with that email and password.
+    return async ({email, password}) => {
+        try {
+            const response = await trackerApi.post('/signin', {email, password});
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({type: 'signin', payload: response.data.token}); //sonuçta ikisi de aynı şeyi yapıyor farklı farklı switch caselerine gerek yok.
 
-        //if we sign in, modify our state, and say that we are authenticated
-
-        //if signing in fails, need to reflect an error message somewhere
+            navigate('mainFlow');
+        } catch (err) {
+            dispatch({
+                type: 'add_error',
+                payload: 'Something went wrong with Sign in'
+            });
+        }
     };
 };
 
@@ -47,6 +61,6 @@ const signout = dispatch => {
 
 export const {Provider, Context} = createDataContext(
     authReducer,
-    {signup, signin, signout},
+    {signup, signin, signout, clearErrorMessage},
     {token: null, errorMessage: ''}
 );
--- task4/src/screens/SignupScreen.js
+++ task4/src/screens/SignupScreen.js
@@ -1,43 +1,26 @@
-import React, {useState, useContext} from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
-import {Text, Input, Button} from 'react-native-elements';
-import Spacer from "../components/Spacer";
+import React, {useContext} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {NavigationEvents} from "react-navigation";
 import {Context as AuthContext} from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SignupScreen = ({navigation}) => {
-    const {state, signup} = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const {state, signup, clearErrorMessage} = useContext(AuthContext);
+
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h4> Sign Up for Unique Experience </Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize={"none"}
-                autoCorrect={false}
+            <NavigationEvents onWillBlur={clearErrorMessage} />
+            <AuthForm
+                headerText="Sign Up for Unique Experience"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer/>
-            <Input
-                secureTextEntry={true} //This is for keeping secure, replacing with dots.
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize={"none"}
-                autoCorrect={false}
+            <NavLink
+                routeName="Signin"
+                text="Already have an account? Sign in instead"
             />
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-            <Spacer>
-                <Button title="Sign Up" onPress={() => signup({email, password})}/>
-            </Spacer>
-            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                <Spacer>
-                    <Text style={styles.link}>Already have an account? Sign in instead</Text>
-                </Spacer>
-            </TouchableOpacity>
         </View>
     );
 };
@@ -58,16 +41,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center', //vertically ortala
         marginBottom: 200
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
-    },
-    link: {
-        color: 'blue',
 
-    }
 });
 
 export default SignupScreen;
