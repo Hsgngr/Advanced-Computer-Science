82085edc88bf24eb5a3da896f7afbf08a4e45390
Ege <hsgngr@gmail.com>
2019-11-29 12:11:20 +0000

    tracker url refreshed

--- task4/src/api/tracker.js
+++ task4/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://f03bed42.ngrok.io' // In every 8 hours ngrok is going to fail to request. We should change this url.
+    baseURL: 'http://e6218eb6.ngrok.io' // In every 8 hours ngrok is going to fail to request. We should change this url.
 });
