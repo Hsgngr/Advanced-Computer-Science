900e30e93493ce9ae7e0f706775285a19de43734
Ege <hsgngr@gmail.com>
2019-11-30 12:31:18 +0000

    Account Screen Done
    Signout is done.

--- task4/src/api/tracker.js
+++ task4/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://e6218eb6.ngrok.io' // In every 8 hours ngrok is going to fail to request. We should change this url.
+    baseURL: 'http://dfe76b71.ngrok.io' // In every 8 hours ngrok is going to fail to request. We should change this url.
 });
