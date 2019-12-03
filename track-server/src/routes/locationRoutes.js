9f6c61ef031fdfccda9fd431944a8d7111673be4
Ege <hsgngr@gmail.com>
2019-12-03 15:03:47 +0000

    dw

--- track-server/src/routes/locationRoutes.js
+++ track-server/src/routes/locationRoutes.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const requireAuth =require('../middlewares/requireAuth');
 
 const locationData = mongoose.model('LocationData');
+const pointData = mongoose.model('PointData');
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.use(requireAuth); //Thats going to ensure that all the different request 
 
 router.get('/locationData', async (req, res) => {
  const userData = await locationData.find({userId: req.user._id});
+
 
  res.send(userData);
 });
@@ -21,14 +23,32 @@ router.post('/locationData', async (req, res) => {
         return res.status(422).send({error: 'You must provide coordinations'});
     }
     try{
+        const houseData = await pointData.find({userId: req.user._id});
         const userData = new locationData({userId: req.user._id, coords});
     await userData.save();
-    res.send('A bunch of points array');
+    mongoose.
+    res.send(houseData);
+    //res.send('A bunch of points array');
     //res.send(userData); // Here it will come the BN Code database results
     } catch (err){
         res.status(422).send({error: err.message})
     }
     
+});
+
+router.post('/locationData2', async (req, res) => {
+
+    if(!req.body){
+        return res.status(422).send({error: 'bisi yanlis'});
+    }
+    try{
+        const houseData = new pointData({Latitude: 1, Longitude: 1, Postcode: 1, AVG_Price: 1});
+        await houseData.save();
+        res.send(houseData);
+    } catch (err){
+        res.status(422).send('BU MU');
+    }
+
 });
 
 module.exports = router;
